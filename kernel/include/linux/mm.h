ac795c836cebdd88559dc1808122fd9a56da78ba
thirdzcee <thirdzcee@gmail.com>
2014-02-18 22:14:30 +0800

    vm max readahead set to 512kb

--- kernel/include/linux/mm.h
+++ kernel/include/linux/mm.h
@@ -1425,7 +1425,11 @@ int write_one_page(struct page *page, int wait);
 void task_dirty_inc(struct task_struct *tsk);
 
 /* readahead.c */
+<<<<<<< HEAD
 #define VM_MAX_READAHEAD	128	/* kbytes */
+=======
+#define VM_MAX_READAHEAD	512	/* kbytes */
+>>>>>>> 7c74957... readahead set to 512kb
 #define VM_MIN_READAHEAD	16	/* kbytes (includes current page) */
 
 int force_page_cache_readahead(struct address_space *mapping, struct file *filp,
